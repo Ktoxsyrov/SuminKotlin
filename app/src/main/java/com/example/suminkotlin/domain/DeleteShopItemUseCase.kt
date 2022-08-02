f6330be5bd5caf153c16cfd3c9d6d506fa113dfb
Ktoxsyrov <ktoxsyrovv@inbox.ru>
2022-08-02 17:20:05 +0300

    domain

---
+++ app/src/main/java/com/example/suminkotlin/domain/DeleteShopItemUseCase.kt
@@ -0,0 +1,8 @@
+package com.example.suminkotlin.domain
+
+class DeleteShopItemUseCase(private val shopListRepository: ShopListRepository) {
+
+    fun deleteShopItem(shopItem: ShopItem){
+        shopListRepository.deleteShopItem(shopItem)
+    }
+}
---
+++ app/src/main/java/com/example/suminkotlin/domain/ShopItem.kt
@@ -0,0 +1,8 @@
+package com.example.suminkotlin.domain
+
+data class ShopItem(
+    val id: Int,
+    val name: String,
+    val count: Int,
+    val selected: Boolean
+)
